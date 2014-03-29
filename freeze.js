5a9cd926f485ea7c91c713a13f430de32290d4ba
kelzha <zhaokelley@gmail.com>
2014-03-29 12:43:17 -0400

    more updates

--- freeze.js
+++ freeze.js
@@ -1,10 +1,11 @@
 //Displays graphics of the numbers 3,2,1 0
 function CountDown(){
-	/*fxFunction(3);*/
+	fxFunction(3);
 	for(var i = 1; i<3;i++){
-		//window.setTimeout(/*fxFunction(3-i)*/,1000);
+		window.setTimeout(fxFunction(3-i),1000);
 	}
-	window.setTimeout(return,1000);
+	window.setTimeout(null,1000);
+
 }
 
 //Takes a freeze frame of the sensor's view  after a 
